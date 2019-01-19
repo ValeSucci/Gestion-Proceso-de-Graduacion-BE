e2dd71257b0e5daa7af92ca718a4d34fa228c65d
Valeria Succi Montenegro <valesuc98@gmail.com>
2019-01-19 02:14:03 -0400

    configuracion db

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    port: process.env.PORT || 9000,
-    db: process.env.MONGODB_URI || 'mongodb://localhost:27017/gestionprocesograduacion',
+    port: process.env.PORT || 7870,
+    db: process.env.MONGODB_URI || 'mongodb://vsucci16:isc9394@127.0.0.1:27017/vsucci16?authSource=vsucci16',
     SECRET_TOKEN: 'GPG'
     
 }
