a9f562204d226c9a1520a931b1da1bd9eb73c11a
Valeria Succi Montenegro <valesuc98@gmail.com>
2019-01-28 01:10:33 -0400

    encriptados

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -87,6 +87,7 @@ function get(req, res) {
         if (error) {
             res.status(500).send(error)
         } else {
+            console.log(user)
             user.password = Encriptador.encode(Encriptador.decode(user.password).substring(0,user.password.toString().length-Token.length));
             console.log(user)
             res.status(200).send(user)
